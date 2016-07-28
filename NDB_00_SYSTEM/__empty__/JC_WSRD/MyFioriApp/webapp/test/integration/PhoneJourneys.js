25a629e979708aa0b348370f18f075c5331d4bb0
Jonathan Cuesto <34cuesto@gmail.com>
2016-07-28 15:58:49 -0400

    se agrego un ejemplo fiori

---
+++ NDB_00_SYSTEM/__empty__/JC_WSRD/MyFioriApp/webapp/test/integration/PhoneJourneys.js
@@ -0,0 +1,21 @@
+jQuery.sap.require("sap.ui.qunit.qunit-css");
+jQuery.sap.require("sap.ui.thirdparty.qunit");
+jQuery.sap.require("sap.ui.qunit.qunit-junit");
+jQuery.sap.require("sap.ui.test.opaQunit");
+jQuery.sap.require("sap.ui.test.Opa5");
+
+jQuery.sap.require("sales.test.integration.pages.Common");
+jQuery.sap.require("sales.test.integration.pages.App");
+jQuery.sap.require("sales.test.integration.pages.Browser");
+jQuery.sap.require("sales.test.integration.pages.Master");
+jQuery.sap.require("sales.test.integration.pages.Detail");
+jQuery.sap.require("sales.test.integration.pages.NotFound");
+
+sap.ui.test.Opa5.extendConfig({
+	arrangements: new sales.test.integration.pages.Common(),
+	viewNamespace: "sales.view."
+});
+
+jQuery.sap.require("sales.test.integration.NavigationJourneyPhone");
+jQuery.sap.require("sales.test.integration.NotFoundJourneyPhone");
+jQuery.sap.require("sales.test.integration.BusyJourneyPhone");
---
+++ NDB_00_SYSTEM/__empty__/JC_WSRD/MyFioriApp/webapp/test/integration/pages/Browser.js
@@ -0,0 +1,91 @@
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"sales/test/integration/pages/Common"
+], function(Opa5, Common) {
+	"use strict";
+
+	Opa5.createPageObjects({
+		onTheBrowserPage: {
+			baseClass: Common,
+
+			actions: {
+
+				iChangeTheHashToObjectN: function(iObjIndex) {
+					return this.waitFor(this.createAWaitForAnEntitySet({
+						entitySet: "Objects",
+						success: function(aEntitySet) {
+							Opa5.getHashChanger().setHash("/doc/" + aEntitySet[iObjIndex].DocEntry);
+						}
+					}));
+				},
+
+				iChangeTheHashToTheRememberedItem: function() {
+					return this.waitFor({
+						success: function() {
+							var sObjectId = this.getContext().currentListItem.getBindingContext().getProperty("DocEntry");
+							Opa5.getHashChanger().setHash("/doc/" + sObjectId);
+						}
+					});
+				},
+
+				iChangeTheHashToTheRememberedId: function() {
+					return this.waitFor({
+						success: function() {
+							var sObjectId = this.getContext().currentId;
+							Opa5.getHashChanger().setHash("/doc/" + sObjectId);
+						}
+					});
+				},
+
+				iChangeTheHashToSomethingInvalid: function() {
+					return this.waitFor({
+						success: function() {
+							Opa5.getHashChanger().setHash("/somethingInvalid");
+						}
+					});
+				}
+
+			},
+
+			assertions: {
+
+				iShouldSeeTheHashForObjectN: function(iObjIndex) {
+					return this.waitFor(this.createAWaitForAnEntitySet({
+						entitySet: "Objects",
+						success: function(aEntitySet) {
+							var oHashChanger = Opa5.getHashChanger(),
+								sHash = oHashChanger.getHash();
+							QUnit.strictEqual(sHash, "doc/" + aEntitySet[iObjIndex].DocEntry, "The Hash is not correct");
+						}
+					}));
+				},
+
+				iShouldSeeTheHashForTheRememberedObject: function() {
+					return this.waitFor({
+						success: function() {
+							var sObjectId = this.getContext().currentListItem.getBindingContext().getProperty("DocEntry"),
+								oHashChanger = Opa5.getHashChanger(),
+								sHash = oHashChanger.getHash();
+							QUnit.strictEqual(sHash, "doc/" + sObjectId, "The Hash is not correct");
+						}
+					});
+				},
+
+				iShouldSeeAnEmptyHash: function() {
+					return this.waitFor({
+						success: function() {
+							var oHashChanger = Opa5.getHashChanger(),
+								sHash = oHashChanger.getHash();
+							QUnit.strictEqual(sHash, "", "The Hash should be empty");
+						},
+						errorMessage: "The Hash is not Correct!"
+					});
+				}
+
+			}
+
+		}
+
+	});
+
+});
